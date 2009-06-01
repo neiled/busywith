c3f92634836dd4c4e8961322a3f4a0e7f4281c36
Neil Edwards <neil@plasticwater.com>
2009-06-01 22:57:11 +0100

    Added AuthLogic and user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe User do
+  before(:each) do
+    @valid_attributes = {
+      :login => "value for login",
+      :crypted_password => "value for crypted_password",
+      :password_salt => "value for password_salt",
+      :persistence_token => "value for persistence_token",
+      :single_access_token => "value for single_access_token",
+      :perishable_token => "value for perishable_token",
+      :login_count => 1,
+      :failed_login_count => 1,
+      :last_request_at => Time.now
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    User.create!(@valid_attributes)
+  end
+end
