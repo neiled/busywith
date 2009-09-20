7d982b7c80a53f00e737d7caa62ac6548caffe3e
Neil Edwards <neil@plasticwater.com>
2009-09-20 15:03:15 +0100

    preparing for live

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 ActionController::Routing::Routes.draw do |map|
+  
   map.resources :teams, :has_many => :projects, :shallow => true 
   map.resources :users, :has_one => :user_status, :except => ["index"]
   map.resources :user_sessions
   map.resources :memberships
-  
+
   map.signin 'signin', :controller => 'user_sessions', :action => "new"
   map.signout 'signout', :controller => 'user_sessions', :action => "destroy"
-  
+
   map.profile '/profile/:login', :controller => 'users', :action => 'show', :login => /.*/
-                                                
+                                              
   map.history '/history/:login', :controller => 'histories', :action => 'show', :login => /.*/, :format => "rss"
-  
+
   map.beta_signup 'beta_signup', :controller => "welcome", :action => 'email_signup'
   
   map.root :controller => "welcome"
