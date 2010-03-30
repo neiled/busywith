49786a0987aef0287009267aae31f74bcd7943df
Neil Edwards <Edwards@mac-pro.config>
2010-03-30 22:01:13 +0100

    added better user status message

--- app/helpers/user_statuses_helper.rb
+++ app/helpers/user_statuses_helper.rb
@@ -1,2 +1,10 @@
 module UserStatusesHelper
+  def user_task_snippet(user)
+    message = user.user_status.current_task_trimmed
+    if(message.length > 45)
+      message + "<img class=\"tooltip\" src=\"/images/more_message.png\" title=\"#{user.user_status.current_task}\"/>"
+    else
+      message
+    end
+  end
 end
--- app/models/user_status.rb
+++ app/models/user_status.rb
@@ -5,7 +5,7 @@ class UserStatus < ActiveRecord::Base
                                         :allow_blank => true
                                         
   validates_presence_of :current_task, :on => :update, :message => "can't be blank"
-  validates_length_of :current_task, :maximum => 50
+  validates_length_of :current_task, :maximum => 1000
   
   belongs_to :active_project, :class_name => "Project", :foreign_key => "project_id"
   
@@ -14,6 +14,14 @@ class UserStatus < ActiveRecord::Base
   before_create :setup_defaults
   
   attr_accessible :current_task, :estimated_completion, :percent_complete, :project_id
+  
+  def current_task_trimmed
+    unless current_task.empty? || current_task.nil?
+      result = current_task.length < 45 ? current_task : current_task[0,45] + "..."
+    else
+      "No Task Set"
+    end    
+  end
   
   private
   
